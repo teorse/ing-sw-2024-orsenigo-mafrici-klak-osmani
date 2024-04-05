a7eb028b9a661d8629863d055c56449ec69a55b9
teorse <matteo2.orsenigo@mail.polimi.it>
2024-04-05 17:22:06 +0200

    Changed Card interface to abstract class

--- Codex_Naturalis/src/main/java/Model/Cards/Card.java
+++ Codex_Naturalis/src/main/java/Model/Cards/Card.java
@@ -1,46 +1,84 @@
 package Model.Cards;
 
+import Model.Cards.Corner;
+import Model.Cards.CornerDirection;
+import Model.Cards.CornerOrientation;
 import Model.Player.CardMap;
 import Model.Utility.*;
 
 import java.util.Map;
 
-/**
- * 
- */
-public interface Card {
+public abstract class Card {
 
+    //ATTRIBUTES
+    private Map<CornerOrientation, Corner> corners;
+
+
+
+
+
+    //CONSTRUCTOR
+    public Card(Map<CornerOrientation, Corner> corners) {
+        this.corners = corners;
+    }
+
+
+
+
+
+    //GETTER
+    public abstract Artifacts getCardColor();
+    public abstract int getPoints();
+    public Map<CornerOrientation, Corner> getCorners() {
+        return corners;
+    }
+
+
+
+
+    //METHODS
     /**
-     *  For CardResource and CardStarter this method returns always true, because these cards don't have constraints,
-     *  while for CardGolden checks if the constraints are satisfied.
+     * For CardResource and CardStarter this method returns always true, because these cards don't have constraints to
+     * play the card, while for CardGolden checks if the constraints are satisfied or not.
      *
      * @param cardMap
      * @return true if the card is placeable on both faces, false if can be placed only face down
      */
-    public boolean isPlaceable(CardMap cardMap);
+    public boolean isPlaceable(CardMap cardMap){
+        return true;
+    }
 
     /**
-     * @param cardMap 
-     * @param coordinates 
-     * @param faceUp 
+     * A generic card gives you points only if is placed faceUp. For CardStarter always returns 0, for CardResource can
+     * be either 0 or 1, while for CardGolden calculate the points using the game rules.
+     *
+     * @param cardMap
+     * @param coordinates
+     * @param faceUp
      * @return the points due to a specific card placement
      */
-    public int countPoints(CardMap cardMap, Coordinates coordinates, boolean faceUp);
+    public abstract int countPoints(CardMap cardMap, Coordinates coordinates, boolean faceUp);
 
     /**
-     * @param faceUp 
-     * @return
+     * This method work in the same way for all the types of card if is placed faceUp, while is different if placed
+     * face down. For CardStarter the attribute centralArtifacts contains the back artifacts, instead for CardResource
+     * the back artifact is always the same as cardColor.
+     *
+     * @param faceUp
+     * @return a Map which represents the total amount of Artifacts on the choosen side
      */
-    public Map<Artifacts, Integer> getAllArtifacts(boolean faceUp);
+    public abstract Map<Artifacts, Integer> getAllArtifacts(boolean faceUp);
 
     /**
-     * @param direction 
-     * @param faceUp 
-     * @return
+     * @param direction
+     * @param faceUp
+     * @return the Artifact one the required corner
      */
-    public Artifacts getCornerArtifact(CornerDirection direction, boolean faceUp);
-
-    public Artifacts getCardColor();
-
-    public int getPoints();
+    public Artifacts getCornerArtifact(CornerDirection direction, boolean faceUp) {
+        CornerOrientation cornerRequired = new CornerOrientation(direction, faceUp);
+        for (CornerOrientation co : this.corners.keySet())
+            if (co.equals(cornerRequired))
+                return this.corners.get(co).getArtifact();
+        return null;
+    };
 }
