73129ff2e006ba428537dbe04cf1bf54fa8e73bc
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-04-07 15:00:35 +0200

    Cleaned up the code and updated the documentation of CardVisibility.

--- Codex_Naturalis/src/main/java/Model/Player/CardVisibility.java
+++ Codex_Naturalis/src/main/java/Model/Player/CardVisibility.java
@@ -11,12 +11,14 @@ import java.util.Map;
  * This class is instanced before playing a card, it stores information about how a card is being played specifically
  * if it's been placed face up or face down and the visibility of its corners when it's being placed and when it will be
  * covered by other cards.
- * This class is then stored in the Model.Player.CardMap.
+ * This class is then stored in the Model.Player.CardMap. and is what the CardMap class will interact with to talk to
+ * the cards.
  */
 
 public class CardVisibility {
+    //ATTRIBUTES
     /**
-     * Reference to the card that this instance of Model.Player.CardPlacement is linked to.
+     * Reference to the card that this instance of CardVisibility is linked to.
      */
     private Card card;
     /**
@@ -26,20 +28,19 @@ public class CardVisibility {
     /**
      *Stores information about the visibility of each of the card's corners.<br>
      *If true, the corner is visible, if false, the corner is covered and not visible.<br>
-     * cornerVisibility[0] is the North-Western corner of the card and the remaining corners in the Array
-     * follow a clockwise pattern, specifically:<br>
-     * cornerVisibility[0] = North-West<br>
-     * cornerVisibility[1] = North-East<br>
-     * cornerVisibility[2] = South-East<br>
-     * cornerVisibility[3] = South-West<br>
      */
     private Map<CornerDirection, Boolean> cornerVisibility;
 
+
+
+
+
+    //CONSTRUCTOR
     /**
      * Default Constructor.<br>
      * Stores information about card's face orientation and sets all corners' visibility to true as by the game's
      * definition when placing a card it can only be placed on top of other cards meaning all of its corners will be visible.
-     * @param card              Card this instance of Model.Player.CardPlacement is being instanced for.
+     * @param card              Card this instance of CardVisibility is being instanced for.
      * @param faceUp            Boolean face orientation of the card, if true card is placed face-up.
      */
     public CardVisibility(Card card, boolean faceUp) {
@@ -54,14 +55,11 @@ public class CardVisibility {
         }};
     }
 
-    /**
-     * Method to update corner visibility once a corner gets covered by a new card being placed on top of it.
-     * @param cornerDirection Position of the corner in the Array
-     */
-    public void coverCorner(CornerDirection cornerDirection){
-        cornerVisibility.put(cornerDirection, false);
-    }
 
+
+
+
+    //GETTERS
     /**
      * Returns visibility value of specified corner.
      * @param cornerDirection int index of corner in the Array cornerVisibility to be returned.
@@ -70,9 +68,22 @@ public class CardVisibility {
     public boolean getCornerVisibility(CornerDirection cornerDirection) {
         return cornerVisibility.get(cornerDirection);
     }
+
+    /**
+     * Method that returns the Artifact that is currently found on the visible side of the card on the specified corner.
+     * @param cornerDirection   Corner from which to get the Artifact.
+     * @return                  Artifact contained in the corner.
+     */
     protected Artifacts getCornerArtifact(CornerDirection cornerDirection){
         return card.getCornerArtifact(cornerDirection, faceUp);
-    };
+    }
+
+    /**
+     * Method that returns a Map containing all artifacts on the visible face of the card.<br>
+     * It is used when updating the counter in CardMap when a new card is placed to get all the new Artifacts all
+     * at once.
+     * @return  Map with all Artifacts on card's visible side.
+     */
     protected Map<Artifacts, Integer> getAllArtifacts(){
         return card.getAllArtifacts(faceUp);
     }
@@ -83,5 +94,18 @@ public class CardVisibility {
      */
     public boolean isFaceUp() {
         return faceUp;
+    }
+
+
+
+
+
+    //SETTERS
+    /**
+     * Method to update corner visibility once a corner gets covered by a new card being placed on top of it.
+     * @param cornerDirection Position of the corner in the Array
+     */
+    public void coverCorner(CornerDirection cornerDirection){
+        cornerVisibility.put(cornerDirection, false);
     }
 }
