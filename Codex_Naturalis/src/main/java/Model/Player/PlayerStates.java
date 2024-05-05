840aeb648296ea46f8e5e1db88eb37a2b623bfbd
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-05-05 15:20:07 +0200

    Added documentation to PlayerStates enum class

--- Codex_Naturalis/src/main/java/Model/Player/PlayerStates.java
+++ Codex_Naturalis/src/main/java/Model/Player/PlayerStates.java
@@ -1,3 +1,31 @@
 package Model.Player;
 
-public enum PlayerStates {PLACE, DRAW, WAIT, PICK_COLOR, PICK_OBJECTIVE}
+/**
+ * Enum representing the possible states of a player during the game.
+ * Each state corresponds to a specific phase or action that the player can take.
+ */
+public enum PlayerStates {
+    /**
+     * State representing the phase where the player is placing cards on the game board.
+     */
+    PLACE,
+
+    /**
+     * State representing the phase where the player is drawing cards from the deck.
+     */
+    DRAW,
+
+    /**
+     * State representing the phase where the player is waiting for a signal from the server.
+     */
+    WAIT,
+
+    /**
+     * State representing the phase where the player is picking a color for themselves in the game.
+     */
+    PICK_COLOR,
+
+    /**
+     * State representing the phase where the player is picking an objective card.
+     */
+    PICK_OBJECTIVE}
