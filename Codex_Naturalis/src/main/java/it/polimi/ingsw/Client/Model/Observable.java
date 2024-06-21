4fdb63597618d53f7b916c000d534f53f56894a5
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-06-21 16:51:27 +0200

    Added logging in Observable class

--- Codex_Naturalis/src/main/java/it/polimi/ingsw/Client/Model/Observable.java
+++ Codex_Naturalis/src/main/java/it/polimi/ingsw/Client/Model/Observable.java
@@ -4,16 +4,20 @@ import it.polimi.ingsw.Client.View.Observer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class Observable {
     //ATTRIBUTES
     protected final List<Observer> observers;
+    private final Logger logger;
 
 
 
 
     //CONSTURCTOR
     protected Observable() {
+        logger = Logger.getLogger(Observable.class.getName());
+        logger.info("Initializing abstract class Observable");
         this.observers = new ArrayList<>();
     }
 
@@ -29,7 +33,19 @@ public abstract class Observable {
         observers.remove(observer);
     }
     protected void updateObservers(){
-        for(Observer observer : observers)
+        logger.info("Updating observers.");
+
+        if(observers.isEmpty())
+            logger.fine("Current observer list is empty");
+        else {
+            logger.fine("Contents of observer list are:");
+            for(Observer observer : observers) {
+                logger.fine("observer: "+ observer.getClass().getName());
+            }
+        }
+        for(Observer observer : observers) {
+            logger.info("Updating observer: "+ observer.getClass().getName());
             observer.update();
+        }
     }
 }
