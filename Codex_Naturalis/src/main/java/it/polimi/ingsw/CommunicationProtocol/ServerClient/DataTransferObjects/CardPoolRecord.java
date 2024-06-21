4cfc3f487525b7020be68b6862fedca6c33da3a7
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-06-21 16:49:02 +0200

    Added missing implements Serializable statement in CardPoolRecord

--- Codex_Naturalis/src/main/java/it/polimi/ingsw/CommunicationProtocol/ServerClient/DataTransferObjects/CardPoolRecord.java
+++ Codex_Naturalis/src/main/java/it/polimi/ingsw/CommunicationProtocol/ServerClient/DataTransferObjects/CardPoolRecord.java
@@ -3,7 +3,8 @@ package it.polimi.ingsw.CommunicationProtocol.ServerClient.DataTransferObjects;
 import it.polimi.ingsw.Server.Model.Game.Artifacts;
 import it.polimi.ingsw.Server.Model.Game.Table.CardPoolTypes;
 
+import java.io.Serializable;
 import java.util.List;
 
-public record CardPoolRecord(Artifacts coveredCardColor, List<CardRecord> visibleCards) {
+public record CardPoolRecord(Artifacts coveredCardColor, List<CardRecord> visibleCards) implements Serializable {
 }
