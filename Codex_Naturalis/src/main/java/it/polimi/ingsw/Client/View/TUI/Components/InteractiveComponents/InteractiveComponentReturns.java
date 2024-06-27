1486fc5bf67db68772fc697edbd380eaac1b0637
Michele-Mafrici <michele.mafrici@mail.polimi.it>
2024-06-27 16:02:09 +0200

    Added javadoc inside ErrorsDictionary and InteractiveComponentReturns classes

--- Codex_Naturalis/src/main/java/it/polimi/ingsw/Client/View/TUI/Components/InteractiveComponents/InteractiveComponentReturns.java
+++ Codex_Naturalis/src/main/java/it/polimi/ingsw/Client/View/TUI/Components/InteractiveComponents/InteractiveComponentReturns.java
@@ -1,7 +1,22 @@
 package it.polimi.ingsw.Client.View.TUI.Components.InteractiveComponents;
 
+/**
+ * Enum representing the possible return statuses of interactive components in the Text User Interface (TUI).
+ */
 public enum InteractiveComponentReturns {
+
+    /**
+     * Indicates that the interactive component has completed its task successfully.
+     */
     COMPLETE,
+
+    /**
+     * Indicates that the interactive component has not completed its task and may need further action.
+     */
     INCOMPLETE,
+
+    /**
+     * Indicates that the user has chosen to quit the interactive component.
+     */
     QUIT
 }
--- Codex_Naturalis/src/main/java/it/polimi/ingsw/CommunicationProtocol/ServerClient/Packets/ErrorsDictionary.java
+++ Codex_Naturalis/src/main/java/it/polimi/ingsw/CommunicationProtocol/ServerClient/Packets/ErrorsDictionary.java
@@ -1,16 +1,56 @@
 package it.polimi.ingsw.CommunicationProtocol.ServerClient.Packets;
 
+/**
+ * Enum representing various error messages that can be sent from the server to the client.
+ */
 public enum ErrorsDictionary {
+    /**
+     * Error indicating that the user is already logged in.
+     */
     YOU_ARE_ALREADY_LOGGED_IN,
+
+    /**
+     * Error indicating that the provided password is incorrect.
+     */
     WRONG_PASSWORD,
+
+    /**
+     * Error indicating that the username was not found.
+     */
     USERNAME_NOT_FOUND,
+
+    /**
+     * Error indicating that the username is already taken.
+     */
     USERNAME_ALREADY_TAKEN,
+
+    /**
+     * Error indicating that the account is already logged in by someone else.
+     */
     ACCOUNT_ALREADY_LOGGED_IN_BY_SOMEONE_ELSE,
 
+    /**
+     * Error indicating that the lobby name is already taken.
+     */
     LOBBY_NAME_ALREADY_TAKEN,
+
+    /**
+     * Error indicating that the lobby size is invalid.
+     */
     INVALID_LOBBY_SIZE,
+
+    /**
+     * Error indicating that the lobby name was not found.
+     */
     LOBBY_NAME_NOT_FOUND,
+
+    /**
+     * Error indicating that the lobby is closed.
+     */
     LOBBY_IS_CLOSED,
 
+    /**
+     * Error indicating a generic error.
+     */
     GENERIC_ERROR,
 }
