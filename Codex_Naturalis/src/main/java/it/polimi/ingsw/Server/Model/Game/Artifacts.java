c1d5d716597f75dedc605feef23fa741f03097f9
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-06-27 14:44:51 +0200

    Added unicode values for icons of Artifacts

--- Codex_Naturalis/src/main/java/it/polimi/ingsw/Server/Model/Game/Artifacts.java
+++ Codex_Naturalis/src/main/java/it/polimi/ingsw/Server/Model/Game/Artifacts.java
@@ -5,21 +5,29 @@ package it.polimi.ingsw.Server.Model.Game;
  */
 //TODO fix the label
 public enum Artifacts {
-    ANIMAL(ArtifactCategories.RESOURCE),
-    FUNGI(ArtifactCategories.RESOURCE),
-    INSECT(ArtifactCategories.RESOURCE),
-    PLANT(ArtifactCategories.RESOURCE),
-    INKWELL(ArtifactCategories.ITEM),
-    MANUSCRIPT(ArtifactCategories.ITEM),
-    QUILL(ArtifactCategories.ITEM),
-    NULL(ArtifactCategories.UTILITY);
+    //ANIMAL(ArtifactCategories.RESOURCE, "\uD83D\uDC3E"),//Blue Paws emoji
+    ANIMAL(ArtifactCategories.RESOURCE, "\uD83D\uDC3A"),//Wolf Face emoji
+    FUNGI(ArtifactCategories.RESOURCE, "\uD83C\uDF44"),
+    INSECT(ArtifactCategories.RESOURCE, "\uD83D\uDC7E"),
+    PLANT(ArtifactCategories.RESOURCE, "\uD83C\uDF3F"),
+    //INKWELL(ArtifactCategories.ITEM, "\uD83E\uDED9"),//Jar emoji
+    INKWELL(ArtifactCategories.ITEM, "\uD83C\uDFFA"),//Vase emoji
+    MANUSCRIPT(ArtifactCategories.ITEM, "\uD83D\uDCDC"),
+    //QUILL(ArtifactCategories.ITEM, "\uD83E\uDEB6"),//Feather emoji
+    QUILL(ArtifactCategories.ITEM, "\uD83D\uDD8B "),//InkPen emoji
+    NULL(ArtifactCategories.UTILITY,"  ");
 
     private final ArtifactCategories categories;
-    private Artifacts(ArtifactCategories categories){
+    private final String unicodeIcon;
+    private Artifacts(ArtifactCategories categories, String unicodeIcon){
         this.categories = categories;
+        this.unicodeIcon = unicodeIcon;
     }
 
     public ArtifactCategories getCategories() {
         return categories;
+    }
+    public String getUnicodeIcon(){
+        return unicodeIcon;
     }
 }
