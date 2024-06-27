8858203afb676bde2f02e30b58ea4d9ffeeb1321
Michele-Mafrici <michele.mafrici@mail.polimi.it>
2024-06-27 16:05:34 +0200

    Added javadoc inside CardPoolRecord and CardVisibilityRecord classes

--- Codex_Naturalis/src/main/java/it/polimi/ingsw/CommunicationProtocol/ServerClient/DataTransferObjects/CardPoolRecord.java
+++ Codex_Naturalis/src/main/java/it/polimi/ingsw/CommunicationProtocol/ServerClient/DataTransferObjects/CardPoolRecord.java
@@ -6,5 +6,9 @@ import it.polimi.ingsw.Server.Model.Game.Table.CardPoolTypes;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * A record representing a pool of cards in the game.
+ * It contains information about the covered card color and a list of visible cards.
+ */
 public record CardPoolRecord(Artifacts coveredCardColor, List<CardRecord> visibleCards) implements Serializable {
 }
